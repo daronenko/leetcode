7b268351d933fae6509f2fbb751f0d4203008fe8
Daniil Mironenko <dan.mironenko@gmail.com>
2023-08-18 15:03:58 +0300

    solve '88. Merge Sorted Array'

---
+++ 88. Merge Sorted Array/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+
+class Solution {
+public:
+    void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
+
