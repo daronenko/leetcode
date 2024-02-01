04c35f3a6884f998c73c964298d1681b37b4f135
Daniil Mironenko <dan.mironenko@gmail.com>
2024-02-01 21:00:52 +0300

    update '215. Kth Largest Element in an Array'

--- 215. Kth Largest Element in an Array/main.cpp
+++ 215. Kth Largest Element in an Array/main.cpp
@@ -21,6 +21,20 @@ class Solution {
 // class Solution {
 //  public:
 //     int findKthLargest(std::vector<int>& nums, int k) {
+//         std::make_heap(nums.begin(), nums.end());
+//         for (int i = 0; i < k - 1; ++i) {
+//             std::pop_heap(nums.begin(), nums.end());
+//             nums.pop_back();
+//         }
+//         std::pop_heap(nums.begin(), nums.end());
+//         return nums.back();
+//     }
+// };
+
+
+// class Solution {
+//  public:
+//     int findKthLargest(std::vector<int>& nums, int k) {
 //         std::priority_queue heap(nums.begin(), nums.end(), std::greater<int>());
 //
 //         while (heap.size() > k) {
