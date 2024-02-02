a85cc56fc84f0c9128628087e6adfb7dd7df5636
Daniil Mironenko <dan.mironenko@gmail.com>
2024-02-02 21:29:38 +0300

    solve '137. Single Number II'

---
+++ 137. Single Number II/main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+
+class Solution {
+ public:
+    int singleNumber(std::vector<int>& nums) {
+        int ones = 0;
+        int twos = 0;
+
+        for (const int num : nums) {
+          ones ^= (num & ~twos);
+          twos ^= (num & ~ones);
+        }
+
+        return ones;
+    }
+};
+
+
+int main() {
+    return 0;
+}
